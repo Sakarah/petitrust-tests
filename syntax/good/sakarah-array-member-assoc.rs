b3e83aedd5634471b0f78e7da638b4325861e5b4
Zetary29 <34010272+Zetary29@users.noreply.github.com>
2017-12-27 19:51:13 +0100

    Changed a test in conformity with rustc 1.14.0

--- syntax/good/sakarah-array-member-assoc.rs
+++ syntax/good/sakarah-array-member-assoc.rs
@@ -1,6 +1,16 @@
+struct S {g : i32}
+struct T {f : Vec<S>}
+struct U {d : i32}
+struct V {b : Vec<i32>}
 fn main()
 {
-    a.b[0];
-    a[0].b;
-    a[0].b[0].c;
+	let a = V{b : vec![42]};
+	a.b[0];
+	let x = U{d : 42};
+	let c = vec![x];
+	c[0].d;
+	let y = S{g : 42};
+	let z = T{f : vec![y]};
+	let e = vec![z];
+	e[0].f[0].g;
 }
